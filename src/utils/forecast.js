aae5f0bc81a7b46d64b0e5fc6c913fa17a0cba2c
HarisCausevic <haris.causevic2910@gmail.com>
2022-02-14 15:27:12 +0100

    New forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to get forecast for given location')
         } else {
-            const strares = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. Feels like ' + body.current.feelslike + ' degrees out. NEW DATA'
+            const strares = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' +body.current.humidity + ' %.'
             callback(undefined, strares)
         }
 
